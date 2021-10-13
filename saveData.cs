aea8236e3c82a9d0c48438dba718d9d7e45c494e
alisterr228 <danil.semenyuk.2013@mail.ru>
2021-10-13 23:16:54 +0800

    new commit1

---
+++ saveData.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+public class saveData : MonoBehaviour
+{
+    private int recordScore;
+
+    public int PubRecordScore
+    {
+        get
+        {
+            return recordScore;
+        }
+        set
+        {
+            if (value >= 0)
+                recordScore = value;
+        }
+    }
+
+    private void Start()
+    {
+        Load();
+    }
+
+    private void Load()
+    {
+        string key = "saveGame";
+
+        if (PlayerPrefs.HasKey(key))
+        {
+            recordScore = PlayerPrefs.GetInt(key);
+        }
+    }
+
+    public void PubSave()
+    {
+        Save();
+    }
+
+    private void Save()
+    {
+        string key = "saveGame";
+
+        PlayerPrefs.SetInt(key, recordScore);
+        PlayerPrefs.Save();
+    }
+
+}
+
