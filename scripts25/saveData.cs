dd24c9efd51d3e51f42b8e717d8f08107ccbf8fb
alisterr228 <danil.semenyuk.2013@mail.ru>
2021-10-13 19:48:03 +0800

    scripts25

---
+++ scripts25/saveData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+public class saveData : MonoBehaviour
+{
+    public int savedInt;
+}
+
